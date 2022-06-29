b75024acdfead4fb561b49a242fb20790cb3f9f0
Raman Zemliakou <raman.zemliakou@gmail.com>
2022-06-29 16:05:50 +0300

    some changes

--- GmailAuthorization/GmailAuthorization/BasePage.cs
+++ GmailAuthorization/GmailAuthorization/BasePage.cs
@@ -17,18 +17,17 @@ namespace GmailAuthorization
         private readonly By _emailField = By.XPath("//input[@type='email']");
         private readonly By _continueButton = By.XPath("//div[@id='identifierNext']//button");
         private readonly By _repeatAttemptButton = By.XPath("//div[@id='next']//button");
-        public BasePage(IWebDriver webDriver)
-        {
-            this.webDriver = webDriver;
-        }
+        public BasePage(IWebDriver webDriver) => this.webDriver = webDriver;
+
         public void EnterEmail()
         {
             var userInfo = new UserInfo();
             webDriver.FindElement(_emailField).SendKeys(userInfo.Email);
+            Thread.Sleep(500);
             webDriver.FindElement(_emailField).SendKeys(Keys.Enter);
-            //WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
-            //wait.Until(ExpectedConditions.ElementToBeClickable(_continueButton));
-            //Thread.Sleep(500);
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(_continueButton));
+            Thread.Sleep(500);
             //webDriver.FindElement(_continueButton).Click();
         }
         public void RepeatLoginAttempt()
--- GmailAuthorization/GmailAuthorization/UnitTest1.cs
+++ GmailAuthorization/GmailAuthorization/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace GmailAuthorization
     public class Tests
     {
         IWebDriver webDriver;
-        public static readonly string Url = "https://accounts.google.com/";
+        public static readonly string Url = "https://gmail.com";
         [SetUp]
         
         public void Setup()
@@ -21,12 +21,13 @@ namespace GmailAuthorization
             options.AddArgument("--safebrowsing-enable-enhanced-protection");
             options.AddArgument("--disable-web-security");
             options.AddArgument("--allow-running-insecure-content");
+            //options.AddArgument("--disable-blink-features = AutomationControlled");
             options.AddArgument("--disable-blink-features=AutomationControlled");
             options.AddUserProfilePreference("safebrowsing.enabled", "true");
             options.AddExcludedArguments("excludeSwitches", "enable-automation");
             options.AddAdditionalCapability("useAutomationExtension", "false");
 
-            options.AddArguments("--disable-web-security", "--disable-gpu", "--proxy-bypass-list=*", "--proxy-server='direct://'", "--log-level=3", "--hide-scrollbars", "--lang=en-us");
+            //options.AddArguments("--disable-web-security", "--disable-gpu", "--proxy-bypass-list=*", "--proxy-server='direct://'", "--log-level=3", "--hide-scrollbars", "--lang=en-us");
             
             webDriver = new ChromeDriver(options);
             webDriver.Manage().Cookies.DeleteAllCookies();
@@ -40,8 +41,8 @@ namespace GmailAuthorization
         {
             var passwordCheck = new ProvidePassword(webDriver);
             passwordCheck.EnterEmail();
-            //passwordCheck.RepeatLoginAttempt();
-            //passwordCheck.EnterEmail();
+            passwordCheck.RepeatLoginAttempt();
+            passwordCheck.EnterEmail();
             passwordCheck.ProvideInvalidPassword();
             var actualMessage = passwordCheck.ReadPasswordValidationMessage();
             var expectedMessage = passwordCheck.ReadExpectedInvalidPasswordValiationMessage();
